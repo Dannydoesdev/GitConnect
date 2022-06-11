d8adc6edde31d9818141310c9abbcb4582389b52
Jericho Sharman <jsharman@hotmail.com.au>
2022-06-11 17:03:55 +1000

    basic server structure

---
+++ server/controllers/sessions.js
@@ -0,0 +1,84 @@
+/* SERVER
+    FILE: SESSIONS API handeler 
+    AUTHOR:  
+    DATE:   
+    DESCRIPTION:*/
+// ********************************************************************************************************************
+// SET UP THE INCLUDES
+const express = require("express");
+const bcrypt = require("bcrypt");
+const db = require("../db/db.js");
+const router = express.Router();
+const BAD_CREDENTIALS = "BAD CREDENTIALS"
+const BAD_CREDENTIALS_STATUS = 403
+// ********************************************************************************************************************
+// CREATE THE ROUTER
+// router.post(`/login`, (req, res) => {
+//   // VALIDATE THE PASSED VARIABLES
+//   const { email, password } = req.body;
+//   if (!email) {
+//     res.status(400).json({ status: "false", message: "Missing email details" });
+//     return;
+//   }
+//   if (!password) {
+//     res.status(400).json({ status: "false", message: "Missing Password" });
+//     return;
+//   }
+//   if (password.length < 10 || password.length > 255) {
+//     res
+//       .status(400)
+//       .json({ status: "false", message: "Incorrect Password length" });
+//     return;
+//   }
+//   console.log("req.sessionID = ", req.sessionID,email); //TODO: delete console.log
+
+//   db.query("SELECT email,userType,profileType,memberShipType,title,firstName,lastName,photo,userLevel,userPcode,hashed_password  FROM users WHERE email = $1;", [email])
+//   .then((dbres) => {
+//     if (req.session.authenticated) {
+//       console.log("User already logged in "); //TODO: delete console.log
+//       res.json(req.session);
+//     } else {
+//       bcrypt.compare(
+//         password + email.toUpperCase(),
+//         dbres.rows[0].hashed_password,
+//         function (err, result) {
+//           if (result) {
+//             delete dbres.rows[0].hashed_password
+//             console.log("The user has successfully logged in"); //TODO: delete console.log
+//             req.session.authenticated = true;
+//             req.session.body = dbres.rows[0];
+//             res.cookie("email", dbres.rows[0].email);
+//             res.cookie("name", dbres.rows[0].firstname, { httpOnly: false });
+//             res.status(200).json(req.session);
+//           } else {
+//             //  Wrong password correct email.
+//             res
+//               .status(BAD_CREDENTIALS_STATUS)
+//               .json({ status: "false", message: BAD_CREDENTIALS });
+//           }
+//         }
+//       );
+//     }
+//   })
+//   .catch((reason) => {
+//     // The user was not found in the database
+//     res.status(BAD_CREDENTIALS_STATUS).json({ status: "false", message: BAD_CREDENTIALS });
+//   });
+// });
+
+
+
+router.get("/", (req, res) => {
+  //  Called on ever page refresh to check if the user is already logged in.
+  // console.log(req.session) //TODO: delete console.log
+  res.json({
+    name: req.session.name,
+    email: req.session.email,
+  });
+});
+router.delete("/", (req, res) => {
+  //  LOG the user OUT. Deletes the session cookie.
+  req.session.destroy();
+  res.json({ success: true });
+});
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,70 @@
+/* SERVER  
+    APPLICATION TITLE: 
+    GROUP:   
+    DATE:   
+    DESCRIPTION:*/
+// ********************************************************************************************************************
+// SET UP THE INCLUDES
+require("dotenv").config();
+const { exit } = require("process");
+const express = require("express");
+const expressSession = require("express-session");
+const pgSession = require("connect-pg-simple")(expressSession);
+const db = require("./db/db");
+// ********************************************************************************************************************
+// CONSTANTS
+const appSecretKey = process.env.EXPRESS_SESSION_SECRET_KEY;
+const PORT = process.env.PORT;
+const app = express(); // Initialise the app
+// ********************************************************************************************************************
+// SET UP THE APP
+app.use("/", (req, res, next) => {
+  // 3 paramaters = middlewear
+  console.log("*************************************************************");
+  console.log(`SERVER COMMUNICATION ${new Date()} ${req.method}`);
+  console.log(`METHOD = ${req.method}`);
+  console.log(`PATH = ${req.path}`);
+  console.log(`PARAMETERS = `);
+  console.log(req.body);
+  console.log(req.session);
+  console.log("*************************************************************");
+  next();
+});
+app.use((err, req, res, next) => {
+  // 4 parameters = error handeler
+  console.log(`I am ERROR middlewear ${new Date()} ${req.method} ${req.path}`);
+  console.log(err);
+  res.status(500).json({ message: "Unknown SERVER/INSERT error occurred" });
+  next();
+});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("client")); // to use the 'client' folder to serve the home html
+app.use(express.json());
+app.use(
+  expressSession({
+    secret: appSecretKey,
+    cookie: { maxAge: 2000000 },
+    resave: false,
+    saveUninitialized: true,
+    store: new pgSession({
+      pool: db,
+      createTableIfMissing: true,
+    }),
+  })
+);
+// const usersController = require("./controllers/users");
+const sessionController = require("./controllers/sessions");
+// app.use("/api/users", usersController);
+app.use("/api/session", sessionController);
+// ********************************************************************************************************************
+// DEVELOPER comms
+if (process.env.DATABASE) {
+  app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+  });
+  console.log(`DATABSE ONLINE: ${process.env.DATABASE}`);
+} else {
+  console.log(
+    "No Database has been setup. Go to the .env file and place the database name"
+  );
+}
