94bcc30146a5b530cdb878faec5d5185d1ac1faf
Jericho Sharman <jsharman@hotmail.com.au>
2022-06-14 23:08:02 +1000

    schema update from my original

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,32 +1,58 @@
--- Is the below required?
-SET client_min_messages = 'error';
-
--- DATABASE TYPES --------------------------------------------------------
-DROP TABLE IF EXISTS profile_settings,usertypes,profileType,users;
-
--- USERS 
-DROP TABLE IF EXISTS tagwords;
-
-
 -- DATABASE TYPES --------------------------------------------------------
 CREATE TABLE usertypes (-- The types of users could be ADMIN, PARTNER,TRAINER,CUSTOMER
-    typeid SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     typename VARCHAR(15) NOT NULL,
     typedescription VARCHAR(50) NOT NULL 
 );
-CREATE TABLE profileType ( -- Public or private
+CREATE TABLE profiletype ( -- Public or private
     id SMALLINT PRIMARY KEY NOT NULL,
     thedescription VARCHAR(15) NOT NULL
+);
+CREATE TABLE socialmediaTypes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    homepage TEXT
+);
+CREATE TABLE programmingLanguages (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
 );
 -- USERS        --------------------------------------------------------
 CREATE TABLE users ( -- All users that have access to Login
     id SERIAL PRIMARY KEY,
+    githubID VARCHAR(20) NOT NULL,
     email TEXT UNIQUE,
-    userType SMALLINT REFERENCES userTypes(typeID) NOT NULL,
+    userType SMALLINT REFERENCES userTypes(id) NOT NULL,
+    profiletype SMALLINT REFERENCES profiletype(id),
     -- title VARCHAR(8) NOT NULL , Not sure if required
     firstName VARCHAR(20)NOT NULL ,
     lastName VARCHAR(30),
     photo TEXT,
-    CONSTRAINT validate_email CHECK (email LIKE '%___@___%__%') ,
+    aboutmetitle TEXT,
+    aboutme TEXT,
+    mobile VARCHAR(16),
+    CONSTRAINT validate_email CHECK (email LIKE '%___@___%__%')
+);
+CREATE TABLE hashed_passwords (
+    id SMALLINT REFERENCES users(id),
     hashed_password TEXT NOT NULL
+);
+CREATE TABLE socialmedialinks (
+    id SMALLINT NOT NULL,
+    socialmediatype SMALLINT REFERENCES socialmediaTypes(id),
+    link TEXT NOT NULL 
+);
+CREATE TABLE userlanguages (
+    userID SMALLINT REFERENCES users(id) NOT NULL,
+    languageID SMALLINT REFERENCES programmingLanguages(id) NOT NULL
+);
+
+-- REPOS..
+CREATE TABLE repoParamaters (
+    repoID TEXT PRIMARY KEY UNIQUE NOT NULL,
+    userID SMALLINT REFERENCES users(id),
+    status BIT,
+    description TEXT,
+    story TEXT,
+    tags TEXT
 );
---
+++ server/db/schema_init.sql
@@ -0,0 +1,12 @@
+-- Is the below required?
+SET client_min_messages = 'error';
+
+-- REPOS
+DROP TABLE IF EXISTS repoParamaters;
+
+-- USERS 
+DROP TABLE IF EXISTS userlanguages,socialmedialinks,hashed_passwords,users;
+
+-- DATABASE TYPES --------------------------------------------------------
+DROP TABLE IF EXISTS programmingLanguages,socialmediaTypes,profiletype,usertypes;
+
