f4028923029f98ca5b913b6269301973e88abb2d
Jericho Sharman <jsharman@hotmail.com.au>
2022-06-15 17:18:54 +1000

    schema

--- server/db/scripts/initialisedb.sh
+++ server/db/scripts/initialisedb.sh
@@ -3,16 +3,16 @@ speaker-test -t sine -f 1000 -l 1
 echo 'This is the FULL database initialisation script. All data will be lost'
 echo -n 'Do you wish to proceed (y/n)?'
 read INPUT
-if [[ ($INPUT == "y")|| ($INPUT == "Y")]]
+if [[ ($INPUT == "y")|| ($INPUT == "Y")]] 
 then
-echo '======================== DROPPING ALL THE DATABASE FILES'
-psql project3_db < server/db/db_init.sql # Drop all tables 
-echo '======================== SETTING THE SCHEMA'
-psql project3_db < server/db/schema.sql # Set the schema
-echo '======================== INSERTING STATIC DATA'
-psql project3_db < server/db/seed_static.sql # Give the static data
-echo '======================== INSERTING DUMMY DATA'
-psql project3_db < server/db/seed_dummy.sql # Seed with any dummy data
+    echo '======================== DROPPING ALL THE DATABASE FILES'
+    psql project3_db < server/db/db_init.sql # Drop all tables 
+    echo '======================== SETTING THE SCHEMA'
+    psql project3_db < server/db/schema.sql # Set the schema
+    echo '======================== INSERTING STATIC DATA'
+    psql project3_db < server/db/seed_static.sql # Give the static data
+    echo '======================== INSERTING DUMMY DATA'
+    psql project3_db < server/db/seed_dummy.sql # Seed with any dummy data
 else
-echo 'OPERATION CANCELLED'
+    echo 'OPERATION CANCELLED'
 fi
