592ef3377f78a20a563aaf06bd4b407f56d1981c
misakigrim <garmonweng@gmail.com>
2022-06-15 23:38:55 +1000

    adding index.js

---
+++ client/js/index.js
@@ -0,0 +1,18 @@
+renderNav()
+
+// const hero = document.createElement('div')
+// hero.style.border = "4px solid #52606D"
+// hero.style.height = "400px"
+// hero.style.width = "800px"
+// hero.style.borderRadius = "50px 50px 0 0"
+// hero.style.margin = "0 auto"
+// main.insertAdjacentElement('afterBegin', hero)
+
+// // login + sign up - in components
+// const login = document.createElement('h2')
+// login.textContent = "Login"
+// const signUp = document.createElement('h2')
+// signUp.textContent = "Sign Up"
+
+// hero.appendChild(login)
+// hero.appendChild(signUp)
