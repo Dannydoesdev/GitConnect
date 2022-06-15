d5b58367b12e1a29355d80ddb126882b512fc722
misakigrim <garmonweng@gmail.com>
2022-06-15 22:37:49 +1000

    split rendernav to comp

--- client/js/index.js
+++ client/js/index.js
@@ -1,21 +1,20 @@
-// generate navigation bar and header - to move into components
-const main = document.getElementById('main')
+renderNav()
 
-// logo top left
-const navBar = document.createElement('div')
-navBar.style.backgroundColor = "#282A35"
-navBar.style.height = "45px"
-
-const browse = document.createElement('h4')
-browse.textContent = "BROWSE"
-browse.style.color = "#ffffff"
-browse.style.margin = "0"
-browse.style.textAlign = "center"
-navBar.appendChild(browse)
-
-main.insertAdjacentElement('afterbegin', navBar)
-
+const hero = document.createElement('div')
+hero.style.border = "4px solid #52606D"
+hero.style.height = "400px"
+hero.style.width = "800px"
+hero.style.borderRadius = "50px 50px 0 0"
+hero.style.margin = "0 auto"
+main.insertAdjacentElement('afterBegin', hero)
 
 // login + sign up - in components
+const login = document.createElement('h2')
+login.textContent = "Login"
+const signUp = document.createElement('h2')
+signUp.textContent = "Sign Up"
+
+hero.appendChild(login)
+hero.appendChild(signUp)
 
 // 
