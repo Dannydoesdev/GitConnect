fa3b80fdb2b950edb73ab0fb7dfaee37185c054b
Jericho Sharman <jsharman@hotmail.com.au>
2022-06-16 23:20:24 +1000

    authenticating github api calls. /gitdetails & /username/repos

--- README.md
+++ README.md
@@ -1,37 +1,70 @@
+
 # GITCONNECT
+endpoints
+-   api/gitconnect/gitdetails 
+-   api/username/repos
 
 ---   
-## DESCRIPTION
+## **DESCRIPTION**
 
 ---   
-## FEATURES
+## **FEATURES**
 
 ---   
-## FUNCTIONALITY
-
+## **FUNCTIONALITY**
 
 ---
-### SECURITY
-Gitconnect server makes the gitHub API calls using a private token for authentication. This token is kept in the .enc file under the label 'TOKEN'
----   
-## GETTING STARTED
+## **SECURITY**
+Gitconnect server makes the gitHub API calls using a private token for authentication. This token is kept in the .enc file under the label 'TOKEN' 
 
-Generating a secret key from the command line. This will be used in conjuction with the postgresql database to security purposes.
+---  
+## **SETTING UP THE SERVER**
+create a file named '.env' in your root project folder and copy and paste the below into this file. 
+```
+PORT=
+DATABASE=
+EXPRESS_SESSION_SECRET_KEY=
+TOKEN=
+```
+*Once completed, continue with the below steps;*
+
+## A       **Choose a vacant port on your local machine and place it in the .env file next to 'PORT='**   
+~~~
+    eg. PORT=3000
+~~~
+
+## B       **Creating & Setting up the database.**    
+1.   Stop the postgresql database if it is running   
+2.   In the terminal window type the following to create the new database.
+~~~
+    createdb project3_db
+~~~
+3.   Be sure to copy the database name and place in the .env file for the DATABASE variable as seen below. replacing 'database_name' with your own database name as created above.   
+~~~
+    DATABASE=project3_db
+~~~  
+
+## C   **Generate a secret key from the CLI. Required**
+1.  Create a random secret key
 ~~~ 
 echo -n 'the seed phrase' | sha256sum
 ~~~
 
-Creating & Setting up the database. 
-### Step 1.
-- Stop the postgresql database if it is running
-- In the terminal window type the following to create the new database.
-~~~ 
-createdb project3_db
+2.  Paste it in your .env file, replacing '\<your key goes here>'
 ~~~
-- Be sure to copy the database name and place in the .env file for the DATABASE variable as seen below. replacing 'database_name' with your own database name as created above.   
+EXPRESS_SESSION_SECRET_KEY=<your key goes here>
 ~~~
-DATABASE=project3_db
-~~~   
+
+## D  **The server will require a git hub TOKEN for its github API calls**   
+1. [follow these instructions to get a token](https://docs.github.com/en/enterprise-server@3.4/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token#creating-a-token)   
+2. Place the token in the .env file using the name 'TOKEN='< copy and paster your new token here >
+
+
+
+
+
+
+   
 ### Step 2
 
 # DEPENDENCIES
--- server/controllers/github.js
+++ server/controllers/github.js
@@ -1,54 +1,120 @@
+/*
+  Github API Caller
+  Written by Jericho Sharman
+  General Assembly 2022
+*/
 const express = require("express");
 const bcrypt = require("bcrypt");
 const db = require("../db/db.js");
 const { default: axios } = require("axios");
 const router = express.Router();
 require("dotenv").config();
-
 // ********************************************************************************************************************
 // CONSTANTS
 const app = express(); // Initialise the app
-const TOKEN = process.env.TOKEN
+const TOKEN = process.env.TOKEN; // Retrieves the TOKEN
 // ********************************************************************************************************************
 
-function isAuthenticated(req, res, next) {
-  if (req.session.authenticated) next();
-  else next("route");
-}
-router.get("/", isAuthenticated, (req, res) => {
-  //  Called on every /session request.
-  // console.log(req.session) //
-  // console.log("req.hostname", req.hostname);
-  // console.log("req.sessionID", req.sessionID);
-  // console.log("req.session.name", req.session.cookie);
-  // console.log("req.session.authenticated", req.session.authenticated);
+// test()  // In conjunction with the test function to test github calls on application startup
+
+//  -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//      BASE Aand AUTHENTICATION ROUTES
+router.get("/", __isAuthenticated, (req, res) => {
   console.log("User is authenticated"); //
   res.json({
     name: req.session.name,
     email: req.session.email,
   });
 });
+
 router.get("/", (req, res) => {
-  //  If the user is not authenticated then come to this route. FIXME: clean up
+  //  Base route
   res.json({
     name: req.session.name,
     email: req.session.email,
     success: false,
   });
 });
-
-router.post(`/repos`, (req, res) => {
-    //  Will send API call to github to pull all the repos by the name passed in from the client
-    const name = req.body.name;
-    axios({
-      method: "get",
-      headers: { Authorization: "token " + TOKEN },
-      url: "https://api.github.com/user/repos",
-    }).then((response) => {
-      console.log(response);
+//  -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//    END POINTS
+//  -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+router.post(`/gitdetails`, __isAuthenticated, (req, res) => {
+  // Get Full github details
+  //  GITHUB API call. Retrieve the user details. Pass the GitHum name in asthe paramater 'name'
+  const name = req.body.name;
+  console.log(`token ${TOKEN}`); // TODO: delete me
+  axios({
+    method: "get",
+    headers: { Authorization: `token ${TOKEN}` },
+    url: `https://api.github.com/users/${name}`,
+  })
+    .then((response) => {
+      // console.log(response, "in /gitconnect/repos");
+      res.json(response.data);
+    })
+    .catch((err) => {
+      __errorHandler(err);
     });
-  res.json({ "hi": name });
+});
+router.post(`/username/repos`, __isAuthenticated, (req, res) => {
+  //get the FULL repo data from GitHub for the username passed in variable 'name'
+  /*   
+   NOTES: NO AUTHENTICATION REQUIRED.
+   Will get the FULL repo data from GitHub for the username passed in variable 'name'
+  */
+  const name = req.body.name;
+  // console.log(`token ${TOKEN}`);
+  axios({
+    method: "get",
+    url: `https://api.github.com/users/${name}/repos`,
+  })
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((err) => {
+      __errorHandler(err, "**************** *********************************");
+    });
+});
+router.post(["/gitdetails", "/username/repos"], (req, res) => {
+  // User not authenticated default POST route
+  //  Base route
+  res.json({
+    message: "User not authenticated",
+    success: false,
+  });
 });
 
+//  -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//            FUNCTIONS
+//  -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+function __isAuthenticated(req, res, next) {
+  // Check to see if the current visitor is authenticated
+  if (req.session.authenticated) next();
+  else next("route");
+}
+function __errorHandler(err, variable) {
+  console.log("ERROR---->", variable, err.message, "");
+}
+function test() {
+  //  Used to test the github calls
+  console.log(
+    `--------------CHECKING REPO CALL FROM GIT HUB with token ${TOKEN}`
+  );
+  axios({
+    method: "get",
+    // headers: { 'Authorization': `token ${TOKEN}` },
+    url: "https://api.github.com/user/repos",
+  })
+    .then((response) => {
+      console.log(response, "in /gitconnect/repos");
+    })
+    .catch((err) => {
+      console.log("REPO ERROR FOUND");
+      __errorHandler(err);
+    });
+  console.log({ hi: "CJUNK" });
+  console.log("REPO CHECK COMPLETED ----------------------");
+}
 
 module.exports = router;
