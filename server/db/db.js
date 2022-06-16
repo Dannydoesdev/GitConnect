62a789c95a580e9d16a13953971d2c26da1b9d9e
Jericho Sharman <jsharman@hotmail.com.au>
2022-06-17 01:22:12 +1000

    testing heroku

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,18 @@
 require("dotenv").config();
 const pg = require("pg"); // Database access module
-const db = new pg.Pool({ database: process.env.DATABASE });
+let db; //= new pg.Pool({ database: process.env.DATABASE });
+if (process.env.NODE_ENV === "production") {
+  db = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  db = new pg.Pool({
+    database: "project3_db",
+    password: "optional_password", // If you have a password on your local db
+  });
+}
 
-
-module.exports =  db
+module.exports = db;
