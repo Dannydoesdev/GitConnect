1171ce4585e3bc84fe25b8c28e9d2e934a13b302
Jericho Sharman <jsharman@hotmail.com.au>
2022-06-18 12:13:39 +1000

    axios

--- server/controllers/github.js
+++ server/controllers/github.js
@@ -6,7 +6,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const db = require("../db/db.js");
-const axios = require("axios");
+// const axios = require("axios");
 const router = express.Router();
 require("dotenv").config();
 // ********************************************************************************************************************
@@ -38,43 +38,43 @@ router.get("/", (req, res) => {
 //  -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 //    END POINTS
 //  -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-router.post(`/gitdetails`, __isAuthenticated, (req, res) => {
-  // Get Full github details
-  //  GITHUB API call. Retrieve the user details. Pass the GitHum name in asthe paramater 'name'
-  const name = req.body.name;
-  console.log(`token ${TOKEN}`); // TODO: delete me
-  axios({
-    method: "get",
-    headers: { Authorization: `token ${TOKEN}` },
-    url: `https://api.github.com/users/${name}`,
-  })
-    .then((response) => {
-      // console.log(response, "in /gitconnect/repos");
-      res.json(response.data);
-    })
-    .catch((err) => {
-      __errorHandler(err);
-    });
-});
-router.post(`/username/repos`, __isAuthenticated, (req, res) => {
-  //get the FULL repo data from GitHub for the username passed in variable 'name'
-  /*   
-   NOTES: NO AUTHENTICATION REQUIRED.
-   Will get the FULL repo data from GitHub for the username passed in variable 'name'
-  */
-  const name = req.body.name;
-  // console.log(`token ${TOKEN}`);
-  axios({
-    method: "get",
-    url: `https://api.github.com/users/${name}/repos`,
-  })
-    .then((response) => {
-      res.json(response.data);
-    })
-    .catch((err) => {
-      __errorHandler(err, "**************** *********************************");
-    });
-});
+// router.post(`/gitdetails`, __isAuthenticated, (req, res) => {
+//   // Get Full github details
+//   //  GITHUB API call. Retrieve the user details. Pass the GitHum name in asthe paramater 'name'
+//   const name = req.body.name;
+//   console.log(`token ${TOKEN}`); // TODO: delete me
+//   axios({
+//     method: "get",
+//     headers: { Authorization: `token ${TOKEN}` },
+//     url: `https://api.github.com/users/${name}`,
+//   })
+//     .then((response) => {
+//       // console.log(response, "in /gitconnect/repos");
+//       res.json(response.data);
+//     })
+//     .catch((err) => {
+//       __errorHandler(err);
+//     });
+// });
+// router.post(`/username/repos`, __isAuthenticated, (req, res) => {
+//   //get the FULL repo data from GitHub for the username passed in variable 'name'
+//   /*   
+//    NOTES: NO AUTHENTICATION REQUIRED.
+//    Will get the FULL repo data from GitHub for the username passed in variable 'name'
+//   */
+//   const name = req.body.name;
+//   // console.log(`token ${TOKEN}`);
+//   axios({
+//     method: "get",
+//     url: `https://api.github.com/users/${name}/repos`,
+//   })
+//     .then((response) => {
+//       res.json(response.data);
+//     })
+//     .catch((err) => {
+//       __errorHandler(err, "**************** *********************************");
+//     });
+// });
 router.post(["/gitdetails", "/username/repos"], (req, res) => {
   // User not authenticated default POST route
   //  Base route
@@ -96,25 +96,25 @@ function __isAuthenticated(req, res, next) {
 function __errorHandler(err, variable) {
   console.log("ERROR---->", variable, err.message, "");
 }
-function test() {
-  //  Used to test the github calls
-  console.log(
-    `--------------CHECKING REPO CALL FROM GIT HUB with token ${TOKEN}`
-  );
-  axios({
-    method: "get",
-    // headers: { 'Authorization': `token ${TOKEN}` },
-    url: "https://api.github.com/user/repos",
-  })
-    .then((response) => {
-      console.log(response, "in /gitconnect/repos");
-    })
-    .catch((err) => {
-      console.log("REPO ERROR FOUND");
-      __errorHandler(err);
-    });
-  console.log({ hi: "CJUNK" });
-  console.log("REPO CHECK COMPLETED ----------------------");
-}
+// function test() {
+//   //  Used to test the github calls
+//   console.log(
+//     `--------------CHECKING REPO CALL FROM GIT HUB with token ${TOKEN}`
+//   );
+//   axios({
+//     method: "get",
+//     // headers: { 'Authorization': `token ${TOKEN}` },
+//     url: "https://api.github.com/user/repos",
+//   })
+//     .then((response) => {
+//       console.log(response, "in /gitconnect/repos");
+//     })
+//     .catch((err) => {
+//       console.log("REPO ERROR FOUND");
+//       __errorHandler(err);
+//     });
+//   console.log({ hi: "CJUNK" });
+//   console.log("REPO CHECK COMPLETED ----------------------");
+// }
 
 module.exports = router;
