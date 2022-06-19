a84f60792bd234fa6cde9c758f4d5c0ebb0d3d56
Jericho Sharman <jsharman@hotmail.com.au>
2022-06-20 03:56:04 +1000

    tidy up

--- client/js/components/Function-whichPageToShow.js
+++ client/js/components/Function-whichPageToShow.js
@@ -1,6 +1,12 @@
+// One stop function to display and render all pages at 1 call. 
+// TODO: the 'case ' numbers below should be more descriptive. Ideally from a common CONSTANTS module.
+
 import { renderFirstTimeRegistration } from "./render-first-time-registration.js";
 import { renderRego } from "./render-rego.js";
 import { renderLogin } from "./render-login.js";
+import { renderProfileEdit } from "./render-profile-edit.js";
+
+
 
 export function whichPageToShow(thePageToShow, gitHubName) {
   switch (thePageToShow) {
@@ -13,6 +19,10 @@ export function whichPageToShow(thePageToShow, gitHubName) {
       break;
     case 3:
       renderLogin();
+      break;
+    case 4:
+      renderProfileEdit();
+      break;
     default:
     // code block
   }
